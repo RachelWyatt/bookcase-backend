cec49ca826b42bc63f5ccca3eaa6ff9d022afa36
Rachel Wyatt <rw126@evansville.edu>
2019-09-02 13:16:18 -0400

    nest books under bookcases

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
    namespace :api do
     namespace :v1 do
-      resources :bookcases  
-      resources :books
+      resources :bookcases do
+        resources :books
+      end
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
