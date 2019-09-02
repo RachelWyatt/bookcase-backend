f472ceba965ecf73d7482b810a237d4f3393a35d
Rachel Wyatt <rw126@evansville.edu>
2019-09-02 13:17:31 -0400

    add index and create actions, strong params for book

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,2 +1,24 @@
-class Api::v1::BooksController < ApplicationController
+class Api::V1::BooksController < ApplicationController
+
+        def index
+            @bookcase = Bookcase.first
+            @books = @bookcase.books
+            render json: @books
+        end
+
+        def create
+            @bookcase = Bookcase.first
+            @book = @bookcase.books.new(bookcase_params)
+            if @book.save
+              render json: @bookcase, status: :created
+            else
+              render json: @bookcase.errors, status: :unprocessable_entity
+            end
+        end
+    
+        private
+        # Never trust parameters from the scary internet, only allow the white list through.
+        def bookcase_params
+          params.require(:book).permit(:title, :author, :page_count, :spine_color)
+        end
 end
