5c6bdb1f8afd917ec6eda1dfc7137f6790a30d84
Rachel Wyatt <rw126@evansville.edu>
2019-09-04 18:52:32 -0400

    add validations to book.rb model for title, author, page_count, and spine_color

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ApplicationRecord
     belongs_to :bookcase 
+
+    validates :title, :author, presence: true
+    validates :page_count, numericality: { only_integer: true }
+    validates :spine_color, format: { with: /\A#?(?:[A-F0-9]{3}){1,2}\z/i, message: "please enter a 6-digit hex code"}
 end
