49ef7982b466011978f338563254e893829003ab
Rachel Wyatt <rw126@evansville.edu>
2019-09-21 11:25:02 -0400

    delete unneeded code for user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::API
-    helpers do
-        def current_user
-            User.first #Mock log in
-        end
 
-        def logged_in?
-            !!current_user
-        end
-    end
 end
